748ebaa30bed5aa553c60225322771d41547e97f
GGalvez <asentch@gmail.com>
2020-12-11 12:43:29 +0100

    mensaje nuevo

--- Algomas.java
+++ Algomas.java
@@ -2,5 +2,8 @@ public class Algomas {
     public static void main(String[] args){
         System.out.println("Hola estoy probando");
         System.out.println("Adios sigo probando xd");
+        System.out.println(
+                "algo masss"
+        );
     }
 }
